c22b4cb4959b8835e0688eae423e9de540574164
Alan Lins <alanblins@gmail.com>
2020-06-19 22:32:50 +0100

    fixed npm start

--- app.js
+++ app.js
@@ -4,6 +4,10 @@
  */
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var errorhandler = require('errorhandler');
+var methodOverride = require('method-override');
 var http = require('http');
 var path = require('path');
 
@@ -11,17 +15,18 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(methodOverride());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/tests')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(errorhandler());
 }
 
 http.createServer(app).listen(app.get('port'), function(){
